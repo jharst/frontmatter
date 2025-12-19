47898ecd7d35d5ae6f1450696026a02faddf14c4
Joachim <122213379+jharst@users.noreply.github.com>
2025-12-19 16:36:33 +0100

    Refactor plugin and add copy with fragment link feature

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,78 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import {App, WorkspaceLeaf, MarkdownView, Notice, Plugin} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class EnhanceWebViewerPlugin extends Plugin {
+	
 	async onload() {
-		await this.loadSettings();
+		// Befehl hinzufügen, um Text und Fragment-Link in die aktive Notiz einzufügen
+	    this.addCommand({
+	      id: "copy-with-fragment",
+	      name: "Copy Selected Text with Fragment Link",
+	      checkCallback: (checking: boolean) => {
+	        if (!checking) {
+	          this.copySelectedTextWithFragment();
+	        }
+	        return true;
+	      },
+	    });
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		 // Text auswählen und Fragment generieren
+  		async copySelectedTextWithFragment(): Promise<void> {
+		    const webViewLeaf = this.getActiveWebViewerLeaf();
+		    if (!webViewLeaf) {
+		      new Notice("No active Web Viewer found!");
+		      return;
+		    }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
+		    // Interaktion mit dem Web Viewer (z. B. um die URL zu erhalten)
+		    const webviewComponent = webViewLeaf.view;
+		    const currentURL = webviewComponent?.state?.url;
+		
+		    // Text aus dem Web Viewer holen
+		    const selectedText = await this.getWebViewerSelection(webViewLeaf);
+		    if (!selectedText || !currentURL) {
+		      new Notice("No text selected or URL missing.");
+		      return;
+		    }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		    // Fragment-Link generieren
+		    const textFragmentURL = `${currentURL}#:~:text=${encodeURIComponent(selectedText)}`;
+		
+		    // Zu aktiver Notiz hinzufügen
+		    const activeLeaf = this.app.workspace.getActiveViewOfType(MarkdownView);
+		    if (!activeLeaf) {
+		      new Notice("No active Markdown note.");
+		      return;
+		    }
+		
+		    activeLeaf.editor.replaceSelection(`[${selectedText}](${textFragmentURL})`);
+		    new Notice("Copied text and link to your note!");
+		}
+
+		  // Helferfunktion: Aktive Web Viewer Leaf holen
+		  getActiveWebViewerLeaf(): WorkspaceLeaf | null {
+			const leaves = this.app.workspace.getLeavesOfType("web-view");
+			return leaves.length > 0 ? leaves[0] : null;
+		  }
+		
+		  // Helferfunktion: Auswahl von Text aus dem Web Viewer abrufen
+		  async getWebViewerSelection(leaf: WorkspaceLeaf): Promise<string | null> {
+			// Dies erfordert eine spezifische Implementierung der Web Viewer API
+			// Placeholder: Annahme, dass eine Methode wie "getSelectedText" existiert.
+			const webviewComponent = leaf.view;
+			if (webviewComponent?.getSelectedText) {
+			  return await webviewComponent.getSelectedText();
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+			return null;
+		  }
 	}
+	
 
 	onunload() {
 	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
+	
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
